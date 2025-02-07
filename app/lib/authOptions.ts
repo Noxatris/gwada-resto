100c398f295d7e394784421e2413a0e3c4753f5f
Noxatris <81755314+Noxatris@users.noreply.github.com>
2025-02-07 00:50:35 -0400

    Refactor authentication setup by moving authOptions to a separate file for better organization and maintainability

---
+++ app/lib/authOptions.ts
@@ -0,0 +1,38 @@
+import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from "next-auth/providers/facebook";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        identifiant: { label: "Identifiant", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        // Remplace par ta logique pour valider les credentials
+        if (
+          credentials?.identifiant === "correctIdentifiant" &&
+          credentials?.password === "correctPassword"
+        ) {
+          return { id: "1", name: "User", email: "user@example.com" };
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/connexion/login",
+  },
+};
